b0c9a822ab32daad1e67897a3001bcc4ebfc67b1
jarvenwang <wang_jin_bao@163.com>
2025-11-15 20:05:48 +0800

    axios请求封装

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,14 +4,14 @@ import { getToken } from '@/utils/auth';
 
 console.log('import.meta.env=', import.meta.env);
 
-// create an axios instance
+// 创建axios实例
 const service = axios.create({
-  baseURL: import.meta.env.VITE_BASE_API, // url = base url + request url
+  baseURL: import.meta.env.VITE_BASE_API, // api的base_url , url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
-  timeout: 5000 // request timeout
+  timeout: 5000 // 请求超时时间
 });
 
-// request interceptor
+// request拦截器
 service.interceptors.request.use(
   config => {
     // do something before request is sent
@@ -31,7 +31,7 @@ service.interceptors.request.use(
   }
 );
 
-// response interceptor
+// respone拦截器
 service.interceptors.response.use(
   /**
    * If you want to get http information such as headers or status
@@ -44,6 +44,10 @@ service.interceptors.response.use(
    * You can also judge the status by HTTP Status Code
    */
   response => {
+    /**
+    * 下面的为通过response自定义code来标示请求状态，当code返回如下情况为权限有问题，登出并返回到登录页
+    * 如通过xmlhttprequest 状态码标识 逻辑可写在下面error中
+    */
     const res = response.data;
 
     // if the custom code is not 20000, it is judged as an error.
